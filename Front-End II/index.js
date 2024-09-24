ad1e0f3f7e113a7f6fe6b18e44e62667d70d96c2
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-09-24 15:09:42 -0400

    .

---
+++ Front-End II/index.js
@@ -0,0 +1,24 @@
+// const body = document.querySelector('body')
+// const ancora = document.createElement('a')
+// ancora.setAttribute('href','https://www.ifro.edu.br')
+// ancora.setAttribute ('target', 'blank')
+// ancora.style.color = "#f00"
+// ancora.style.textDecoration ="none"
+
+// ancora.textContent = 'IFRO'
+// body.appendChild(ancora)
+// const h1 = document.querySelector('h1')
+// h1.remove()
+
+const body = document.querySelector('body')
+const lista = document.createElement('ul')
+const item1 = document.createElement('li')
+const item2 = document.createElement('li')
+const item3= document.createElement('li')
+item1.textContent = 'vnkjv'
+item2.textContent = 'vnkjv'
+item3.textContent = 'vnkjv'
+lista.append(item1)
+lista.append(item2)
+lista.append(item3)
+body.append(lista)
