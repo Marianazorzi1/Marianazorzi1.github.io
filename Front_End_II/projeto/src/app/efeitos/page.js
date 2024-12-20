ef18ebab9b11e55e22457d8020f243178ceb8695
Marianazorzi1 <m.zorzi@estudante.ifro.edu.br>
2024-12-20 08:20:13 -0400

    .

--- Front_End_II/projeto/src/app/efeitos/page.js
+++ Front_End_II/projeto/src/app/efeitos/page.js
@@ -1,57 +1,76 @@
 'use client';
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function Efeitos(){
-    // const [cont, setCont] = useState(0);
-    // const [cont2, setCont2] = useState(0);
-    // useEffect(()=> {
-    //     console.log('Renderizou')
-    // },[cont, cont2])
-
+export default function Efeitos() {
     const [ufs, setUfs] = useState([]);
+    const [cid, setCid] = useState([]);
     const [ufSelected, setUfSelected] = useState('');
+    const [cidSelected, setCidSelected] = useState('');
 
-    const getUfs = async ()=> {
-        try{
+    const getUfs = async () => {
+        try {
             const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
-            if(!response.ok){
-                throw new Error('Erro ao buscar dados: '+ response.statusText);
+            if (!response.ok) {
+                throw new Error('Erro ao buscar dados: ' + response.statusText);
             }
             const data = await response.json();
             setUfs(data);
-            console.log(data);
-
-        }catch(error){
-            console.log('Ocorreu algum erro: ' + error)
+        } catch (error) {
+            console.log('Ocorreu algum erro: ' + error);
         }
+    };
 
-    }
-
-    useEffect(()=> {
+    useEffect(() => {
         getUfs();
-    },[])
+    }, []);
+
+    const getCid = async () => {
+        try {
+            const response = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufSelected}/municipios?orderBy=nome`);
+            if (!response.ok) {
+                throw new Error('Erro ao buscar dados: ' + response.statusText);
+            }
+            const data = await response.json();
+            setCid(data);
+        } catch (error) {
+            console.log('Ocorreu algum erro: ' + error);
+        }
+    };
+
+    useEffect(() => {
+        if (ufSelected) getCid();
+    }, [ufSelected]);
 
     return (
         <div>
             <h1>Efeitos Colaterais / Use Effect</h1>
-            {
-                <select onChange={(ev) => setUfSelected(ev.target.value)}>
-                    <option value="">Selecione o estado</option>
-                    {ufs.map((uf) => (
-                        <option
-                            value={uf.id}
-                            key={uf.id}>
-                                {uf.nome}
-                        </option>
-                    ))}
-                </select>
-            }
 
-            {/* <button onClick={() => (setCont(cont + 1))}>Adicionar</button>
-            <p>Renderizações cont 1: {cont}</p>
-            <button onClick={() => (setCont2(cont2 + 1))}>Adicionar</button> 
-             <p>Renderizações cont 2: {cont2}</p>   */}
+            
+            <select onChange={(e) => {setUfSelected(e.target.value), setCidSelected('')}}>
+                <option value="">Selecione o estado</option>
+                {ufs.map((uf) => (
+                    <option value={uf.sigla} key={uf.id}>
+                        {uf.nome}
+                    </option>
+                ))}
+            </select>
 
+            
+            <select onChange={(e) => setCidSelected(e.target.value)}>
+                <option value="">Selecione a cidade</option>
+                {cid.map((ci) => (
+                    <option value={ci.nome} key={ci.id}>
+                        {ci.nome}
+                    </option>
+                ))}
+            </select>
+
+          
+            
+                {cidSelected && <p> {cidSelected}</p>}
+                
+
+            
         </div>
-    )
+    );
 }
