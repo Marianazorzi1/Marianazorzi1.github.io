6165ef346f2e1e2e3dfdb8157f044d8e0ecc80a5
Mariana Zorzi <teste@teste.com>
2025-02-24 19:10:25 -0400

    Mari

--- Front_End_II/Clinica/src/app/page.js
+++ Front_End_II/Clinica/src/app/page.js
@@ -7,15 +7,15 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.titulo}>
-        <h1 className={styles.title}>Bem-vindo à Clínica Vida Saudável</h1>
+        <h1 className={styles.title}>Bem-vindo ao Centro Médico Zorzi</h1>
       </div>
       <main className={styles.main}>
         <p className={styles.text}>
-          Na Clínica Vida Saudável, sua saúde é nossa prioridade. Nossa equipe de profissionais e tecnologia de ponta garantem um atendimento humanizado e de excelência. Aqui você encontra serviços que vão desde consultas médicas até tratamentos especializados, sempre focados em promover bem-estar e qualidade de vida para você e sua família.
+          No Centro Médico Zorzi, sua saúde é nossa prioridade. Nossa equipe de profissionais e tecnologia de ponta garantem um atendimento humanizado e de excelência. Aqui você encontra serviços que vão desde consultas médicas até tratamentos especializados, sempre focados em promover bem-estar e qualidade de vida para você e sua família.
         </p>
       </main>
       <footer className={styles.footer}>
-        <p className={styles.footerText}>© 2025 Clínica Vida Saudável. Todos os direitos reservados.</p>
+        <p className={styles.footerText}>© 2025 Centro Médico Zorzi. Todos os direitos reservados.</p>
       </footer>
     </div>
   );
